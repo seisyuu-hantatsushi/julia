cfd25902bb45db04f2ab8196cec5d486bc8806bd
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2020-05-08 01:25:04 +0900

    Gillepie algorithmを使ったSIR Modelのシミュレーション

---
+++ epidemic_model/sir_gillespie/sirmodel.jl
@@ -0,0 +1,35 @@
+using Plots
+using DifferentialEquations
+using DiffEqBiological
+
+sirmodel = @reaction_network SIR begin
+    β, S + I --> 2I
+    γ, I --> R
+end β γ
+
+
+function solveSIR(u0, β, γ, time_span)
+
+    p = [β, γ]
+
+    dprob = DiscreteProblem(sirmodel, u0, time_span, p)
+    jprob = JumpProblem(dprob, Direct(), sirmodel)
+
+    sol = solve(jprob, SSAStepper())
+
+    return sol
+
+end
+
+R0=2.0
+N=1000
+γ=0.1
+β=γ*R0/N
+
+sol = solveSIR([N-1,1,0], β, γ, (0, 160.0))
+
+plot_graph = plot(sol, vars=(0,1), title="SIR model/Gillespie", label="Suscepible", color=:blue, legend=:left)
+plot!(plot_graph, sol, vars=(0,2), label="Infectious", color=:red)
+plot!(plot_graph, sol, vars=(0,3), label="Removed", color=:green)
+
+png(plot_graph, "sir_gillespie.png")
