96988b5b124706780ca90ca0528fd22fc2c519b7
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2020-05-10 14:46:53 +0900

    graphのラベルを間違えていたので修正.

--- epidemic_model/seir_model/seir_gillespie_model.jl
+++ epidemic_model/seir_model/seir_gillespie_model.jl
@@ -29,8 +29,8 @@ sol = solveSEIR([N-1,0,1,0], β, γ, δ, (0.0,360.0))
 
 plot_graph = plot(sol, vars=(0,1), title="SEIR model/Gillespie R0=$R0 γ=$γ δ=$δ", label="Suscepible",
                   color=:blue, seriestype=:line, legend=:left, size=(1200,900))
-plot!(plot_graph, sol, vars=(0,2), label="Exposed", color=:darkmagenta)
-plot!(plot_graph, sol, vars=(0,3), label="Infectious", color=:red)
+plot!(plot_graph, sol, vars=(0,2), label="Infectious", color=:red)
+plot!(plot_graph, sol, vars=(0,3), label="Exposed", color=:darkmagenta)
 plot!(plot_graph, sol, vars=(0,4), label="Removed", color=:green)
 
 png(plot_graph, "seir_gillespie.png")
