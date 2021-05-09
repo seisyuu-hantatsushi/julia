48c9ea96a0eaa5b3a02f8d4def6352f38a81fa6a
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-05-09 13:12:56 +0900

    窓関数名を間違えていたので修正.

--- analog_transmisson/am_fft.jl
+++ analog_transmisson/am_fft.jl
@@ -13,7 +13,7 @@ function sqaure_windowing_signal(signal::Array{Float64})
     return (windowed_signal,window_correction_factor)
 end
 
-function hamming_windowing_signal(signal::Array{Float64})
+function hanning_windowing_signal(signal::Array{Float64})
     windowed_signal::Array{Float64} = []
     window::Array{Float64} = []
     total_window_power = 0
@@ -44,7 +44,7 @@ function main()
     println(length(w_m_vals))
 
     #(windowed_signal, window_correction_factor)  = sqaure_windowing_signal(w_m_vals[1:window_size])
-    (windowed_signal, window_correction_factor) = hamming_windowing_signal(w_m_vals[1:window_size])
+    (windowed_signal, window_correction_factor) = hanning_windowing_signal(w_m_vals[1:window_size])
     println(window_correction_factor)
     powers = map(x->20*log10(abs(x)*window_correction_factor),fft(windowed_signal)|>fftshift)
     freqs = fftfreq( window_size, sampling_rate )|>fftshift
