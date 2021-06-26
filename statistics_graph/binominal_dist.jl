d1cdd1b6232ee7572bf0778bd3799b44dcf9832a
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-06-26 14:24:43 +0900

    graph parameter are collected in array.

--- statistics_graph/binominal_dist.jl
+++ statistics_graph/binominal_dist.jl
@@ -1,6 +1,6 @@
 using ArgParse,CairoMakie
 
-CairoMakie.activate!(type="png")
+CairoMakie.activate!(type="pdf")
 
 function parse_argument()
 
@@ -12,31 +12,25 @@ end
 
 function main()
 
+    xs_max = 50
     fig = Figure(resolution = (800, 600))
-    xs = 0:50
+    xs = 0:xs_max
 
-    p1_s::Array{Float64} = []
-    for x in xs
-        push!(p1_s,binominal_prob_desity(50,0.1,x))
+    Axis(fig[1,1], title = "binominal probability mass function")
+
+    graph_params = [Dict("prob"=>0.1, "color"=>:gray0,  "marker"=>:circle),
+                    Dict("prob"=>0.5, "color"=>:gray30, "marker"=>:diamond),
+                    Dict("prob"=>0.7, "color"=>:gray50, "marker"=>:rect)]
+
+    for params in graph_params
+        p_s::Array{Float64} = []
+        for x in xs
+            push!(p_s,binominal_prob_desity(xs_max,params["prob"],x))
+        end
+        stem!(fig[1,1], xs, p_s, color = params["color"], marker = params["marker"])
     end
 
-    lines(fig[1,1], xs, p1_s, color = :blue)
-
-    p2_s::Array{Float64} = []
-    for x in xs
-        push!(p2_s,binominal_prob_desity(50,0.5,x))
-    end
-
-    lines!(fig[1,1], xs, p2_s, color = :red)
-
-    p3_s::Array{Float64} = []
-    for x in xs
-        push!(p3_s,binominal_prob_desity(50,0.7,x))
-    end
-
-    lines!(fig[1,1], xs, p3_s, color = :yellow)
-
-    save("plot.png", fig)
+    save("plot.pdf", fig)
 
 end
 
