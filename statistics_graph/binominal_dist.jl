d770acb6cea26b9e68f504496c6b6bf0202ff4c9
Kazushige Taniguchi <taniguchi.kazushige@gmail.com>
2021-06-26 14:33:38 +0900

    add legend

--- statistics_graph/binominal_dist.jl
+++ statistics_graph/binominal_dist.jl
@@ -16,20 +16,21 @@ function main()
     fig = Figure(resolution = (800, 600))
     xs = 0:xs_max
 
-    Axis(fig[1,1], title = "binominal probability mass function")
+    axis = Axis(fig[1,1], title = "binominal probability mass function")
 
-    graph_params = [Dict("prob"=>0.1, "color"=>:gray0,  "marker"=>:circle),
-                    Dict("prob"=>0.5, "color"=>:gray30, "marker"=>:diamond),
-                    Dict("prob"=>0.7, "color"=>:gray50, "marker"=>:rect)]
+    graph_params = [Dict("prob"=>0.1, "color"=>:gray0,  "marker"=>:circle,  "label"=>"p=0.1"),
+                    Dict("prob"=>0.5, "color"=>:gray30, "marker"=>:diamond, "label"=>"p=0.5"),
+                    Dict("prob"=>0.7, "color"=>:gray50, "marker"=>:rect,    "label"=>"p=0.7")]
 
     for params in graph_params
         p_s::Array{Float64} = []
         for x in xs
             push!(p_s,binominal_prob_desity(xs_max,params["prob"],x))
         end
-        stem!(fig[1,1], xs, p_s, color = params["color"], marker = params["marker"])
+        stem!(fig[1,1], xs, p_s, color = params["color"], marker = params["marker"], label=params["label"])
     end
 
+    axislegend(axis)
     save("plot.pdf", fig)
 
 end
