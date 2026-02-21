e911f977f99a933e94c47c4707a0330046efb1ae
TANIGUCHI Kazushige <taniguchi.kazushige@gmail.com>
2026-02-21 15:46:34 +0900

    juliaの標準関数で置き換え.

--- tsss/chapter_1/nikkei225_diff_graph.jl
+++ tsss/chapter_1/nikkei225_diff_graph.jl
@@ -1,21 +1,24 @@
 using CSV,DataFrames
 using CairoMakie
 
-include("../lib/tsss.jl")
+#include("../lib/tsss.jl")
 
-nikkei255_data = CSV.read("../R_scripts/csv_out//Nikkei225.csv", DataFrame)
-nikkei255_data_log10 = transform(nikkei255_data, :value => (v -> log10.(Float64.(v))) => :value)
+nikkei225_data = CSV.read("../R_scripts/csv_out//Nikkei225.csv", DataFrame)
+nikkei225_data_log10 = transform(nikkei225_data, :value => (v -> log10.(Float64.(v))) => :value)
 
-print(nikkei255_data_log10)
+nikkei225_data.dv = vcat(missing, diff(nikkei255_data.value))
+nikkei225_data = dropmissing(nikkei225_data, [:dv])
 
-nikkei255_diff_data = diff_timeseries(nikkei255_data)
+nikkei225_data_log10.dv = vcat(missing, diff(nikkei255_data_log10.value))
+nikkei225_data_log10 = dropmissing(nikkei225_data_log10, [:dv])
 
-nikkei255_diff_data_log10 = diff_timeseries(nikkei255_data_log10)
+print(nikkei225_data)
+print(nikkei225_data_log10)
 
 fig = Figure(size = (900, 450))
 ax = Axis(fig[1, 1], xlabel = "time (year)", ylabel = "value", title = "value vs time")
-lines!(ax, nikkei255_diff_data.t, nikkei255_diff_data.dv; linewidth = 0.6)
+lines!(ax, nikkei225_data.time, nikkei225_data.dv; linewidth = 0.6)
 bx = Axis(fig[2, 1], xlabel = "time (year)", ylabel = "log10 value", title = "value vs time")
-lines!(bx, nikkei255_diff_data_log10.t, nikkei255_diff_data_log10.dv; linewidth = 0.6)
+lines!(bx, nikkei225_data_log10.time, nikkei225_data_log10.dv; linewidth = 0.6)
 
 fig
